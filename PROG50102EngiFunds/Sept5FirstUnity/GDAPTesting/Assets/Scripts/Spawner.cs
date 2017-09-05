e7f6a592ed7e03505973fd837b7b064779c93cf0
Adsew <30912391+Adsew@users.noreply.github.com>
2017-09-05 13:51:31 -0400

    FundEngi
    More unity stuff done

---
+++ PROG50102EngiFunds/Sept5FirstUnity/GDAPTesting/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject obj;
+    public KeyCode keyToCheck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    private void SpawnerUpdate() {
+        
+        if (Input.GetKeyDown (keyToCheck) == true) {
+            
+            //make a copy
+            if (obj != null) {
+
+                Instantiate(obj);
+            }
+            //Debug.Log("SpawnerUpdate:: Object is null");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        SpawnerUpdate();
+	}
+}
