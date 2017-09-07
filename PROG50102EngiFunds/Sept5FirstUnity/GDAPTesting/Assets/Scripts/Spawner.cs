9a7a6fa6912b12fc62df5fad5f5f77537ae8a7c0
Adsew <30912391+Adsew@users.noreply.github.com>
2017-09-07 12:50:11 -0400

    EngFund Sept 7
    Sept 7 class work

--- PROG50102EngiFunds/Sept5FirstUnity/GDAPTesting/Assets/Scripts/Spawner.cs
+++ PROG50102EngiFunds/Sept5FirstUnity/GDAPTesting/Assets/Scripts/Spawner.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Spawner : MonoBehaviour {
 
     public GameObject obj;
     public KeyCode keyToCheck;
+    private GameObject spawnLoc;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        //spawnLoc = GameObject.FindGameObjectWithTag("SpawnLoc");
+        IsSpawnLoc s1 = this.gameObject.GetComponentInChildren<IsSpawnLoc>();
+
+        if (s1 != null) {
+
+            spawnLoc = s1.gameObject;
+        }
+        
+        Debug.Log("Start is finished.");
 	}
 	
     private void SpawnerUpdate() {
@@ -17,9 +28,9 @@ public class Spawner : MonoBehaviour {
         if (Input.GetKeyDown (keyToCheck) == true) {
             
             //make a copy
-            if (obj != null) {
-
-                Instantiate(obj);
+            if (obj != null && spawnLoc != null) {
+                //this.gameObject.transform.position + new Vector3.forward
+                Instantiate(obj, spawnLoc.transform.position, spawnLoc.transform.rotation);
             }
             //Debug.Log("SpawnerUpdate:: Object is null");
         }
---
+++ PROG50102EngiFunds/Sept5FirstUnity/GDAPTesting/Assets/Scripts/TargetLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLogic : MonoBehaviour {
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<IsProjectile>() != null) //fix this
+        {
+            Destroy(this.gameObject); // this is the script component
+        }
+        
+        //Somehting hit me do somehting
+        Debug.Log("I was hit by: " + col.gameObject.name);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
