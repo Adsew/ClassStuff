a2e7b09e4f51db35d22500fe28e1d012fe881990
Adsew <30912391+Adsew@users.noreply.github.com>
2017-09-12 11:01:55 -0400

    Game Arch sept12
    lesson, organize, cleanup

---
+++ PROG50016GameArch/InClassLessons/Sept12/Source.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <time.h>
+
+void sizeofLook() {
+
+    std::cout << "bool " << sizeof(bool) << "\n";
+    std::cout << "char " << sizeof(char) << "\n";
+    std::cout << "short " << sizeof(short) << "\n";
+    std::cout << "int " << sizeof(int) << "\n";
+    std::cout << "float " << sizeof(float) << "\n";
+    std::cout << "double " << sizeof(double) << "\n";
+    std::cout << "long " << sizeof(long) << "\n";
+    std::cout << "longlong " << sizeof(long long) << "\n";
+
+    std::cout << "\n";
+}
+
+void randomGenerator() {
+
+    int num[10];
+    int i = 0;
+
+    srand((unsigned)time(0));
+
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++) {
+
+        num[i] = (rand() % 100) + 1;
+    }
+
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++) {
+
+        std::cout << num[i] << " ";
+    }
+    
+    std::cout << "\n";
+}
+
+int main(int argc, char *argv[]) {
+
+    sizeofLook();
+
+    randomGenerator();
+
+    return 0;
+}
