759f5f9ec713aed50feaddec3352c8be60a3ac08
Adsew <30912391+Adsew@users.noreply.github.com>
2017-09-18 09:41:44 -0400

    Tools In class a1 done

---
+++ PROG56693ToolsDataDesign/Sept18 In class Assignment 1/JamesBrooksSplinesA1/Assets/Scripts/Spline.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spline : MonoBehaviour {
+
+    [Range(0,1)]
+    public float t;
+
+    public List<GameObject> ContPoints;
+
+    public GameObject head;
+
+    void CatmullRomSpline()
+    {
+        Vector3 newPosition;
+
+        Vector3 p0 = ContPoints[0].transform.position;
+        Vector3 p1 = ContPoints[1].transform.position;
+        Vector3 p2 = ContPoints[2].transform.position;
+        Vector3 p3 = ContPoints[3].transform.position;
+
+        //Catmull rom equation converted
+        newPosition = 0.5f * (
+            (p1 * 2.0f)
+            + (p2 - p0) * t
+            + (2.0f * p0 - 5.0f * p1 + 4.0f * p2 - p3) * t * t
+            + (3.0f * p1 - p0 - 3.0f * p2 + p3) * t * t * t
+            );
+
+        head.transform.SetPositionAndRotation(newPosition, head.transform.rotation);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CatmullRomSpline();
+	}
+}
