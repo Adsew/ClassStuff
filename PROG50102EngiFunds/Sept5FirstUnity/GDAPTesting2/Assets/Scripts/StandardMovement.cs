f3c85e8490bd3e3a1cdfe7d53e5c057d80db0cc3
Adsew <30912391+Adsew@users.noreply.github.com>
2017-10-03 13:51:00 -0400

    EngiFund lesson

---
+++ PROG50102EngiFunds/Sept5FirstUnity/GDAPTesting2/Assets/Scripts/StandardMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardMovement : MonoBehaviour {
+
+    // Script communicating with an animator
+
+    private Animator anim;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        anim = this.gameObject.GetComponent<Animator>();
+	}
+
+    void HeroMovementUpdate() {
+
+        
+    }
+
+    void HeroAnimationUpdate() {
+
+        // Set movements to controller input etc.
+        if (anim != null) {
+            
+            // SHOULD USE THIS THROUGH INPUT MANAGER ALLOWS DESIGN TIME MODIFICATION WITHOUT CHANIGNG CODE
+            if (Input.GetButtonDown("Jump")) {
+
+                anim.SetTrigger("Jump");
+            }
+
+            if (Input.GetKeyDown(KeyCode.T)) {
+
+                anim.SetTrigger("Dance");
+            }
+
+            if (Input.GetButtonDown("Fire1")) {
+
+                anim.SetTrigger("LightPunch");
+            }
+
+            if (Input.GetButton("Vertical")) {
+
+                anim.SetBool("Walking", true);
+            }
+            else {
+
+                anim.SetBool("Walking", false);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        HeroMovementUpdate();
+        HeroAnimationUpdate();
+	}
+}
---
+++ PROG56693ToolsDataDesign/MidtermProject/JamesBrooksSplineTool/Assets/Tools/SplineCreator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SplineCreator {
+
+    /*
+     * THINGS TO IMPLEMENTS
+     * 
+     * Adding spline
+     * adding spline node to created spline
+     * setting start of spline
+     * loading splines back into tool on startup
+     * saveing loading splines to xml
+     * Draw line with debug mode
+    */
+    
+    List<GameObject> splines;
+    
+    [MenuItem("JB Tools/Spline/New Spline")]
+    public static void CreateNewSpline() {
+
+        TextDialogueWindow window = new TextDialogueWindow();
+        window.GetSingleText("New Spline Name");
+
+        Debug.Log(name);
+
+        //GameObject emptySpline = new GameObject("My spline");
+        //emptySpline.AddComponent<Spline>();
+    }
+
+
+    /***** Singleton Classes for Editor Interaction *****/
+
+
+    // Class that can get a line of text from the user using a popup window
+    class TextDialogueWindow : ScriptableWizard {
+
+        public string textField;
+
+        public bool windowClosed;
+
+        public void GetSingleText(string windowText) {
+
+            ScriptableWizard.DisplayWizard<TextDialogueWindow>(windowText, "Accept");
+
+            windowClosed = false;
+            textField = "";
+        }
+
+        private void OnWizardCreate() {
+
+            windowClosed = true;
+        }
+    }
+}
