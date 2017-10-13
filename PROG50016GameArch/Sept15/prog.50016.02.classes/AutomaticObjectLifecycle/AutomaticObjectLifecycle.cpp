1282c815b78e80ac969fdb89134ae914cad3ca7c
Adsew <30912391+Adsew@users.noreply.github.com>
2017-10-12 20:42:40 -0400

    Arch studying

--- PROG50016GameArch/Sept15/prog.50016.02.classes/AutomaticObjectLifecycle/AutomaticObjectLifecycle.cpp
+++ PROG50016GameArch/Sept15/prog.50016.02.classes/AutomaticObjectLifecycle/AutomaticObjectLifecycle.cpp
@@ -7,9 +7,18 @@
 //
 class VerboseObject 
 {
+protected:
+    
     std::string mName;
 
 public:
+
+    VerboseObject() {
+
+        mName = "No Name Given";
+        std::cout << "*** Creating default " << mName << std::endl;
+    }
+
     // constructor
     VerboseObject(std::string name)
     {
@@ -18,21 +27,72 @@ public:
     }
 
     // destructor
-    ~VerboseObject()
+    virtual ~VerboseObject()
     {
-        std::cout << "*** Destroying " << mName << std::endl;
+        std::cout << "*** Destroying Verbose Object " << mName << std::endl;
+    }
+
+    virtual void greet() const
+    {
+        std::cout << mName << " says hi" << std::endl;
+    }
+};
+
+class DerivedObject : public VerboseObject {
+
+public:
+
+    DerivedObject() {
+
+        std::cout << "*** Creating derived " << mName << std::endl;
+    }
+
+    DerivedObject(std::string name) : VerboseObject(name) {
+
+        std::cout << "*** Creating derived with name " << mName << std::endl;
+    }
+
+    ~DerivedObject() {
+
+        std::cout << "*** Destroying Derived Object " << mName << std::endl;
     }
 
     void greet() const
     {
-        std::cout << mName << " says hi" << std::endl;
+        std::cout << mName << " says hi derived" << std::endl;
+    }
+};
+
+class Derived2Object : public DerivedObject {
+
+public:
+
+    Derived2Object() {
+
+        std::cout << "*** Creating derived2 " << mName << std::endl;
+    }
+
+    Derived2Object(std::string name) : DerivedObject(name) {
+
+        std::cout << "*** Creating derived2 with name " << mName << std::endl;
+    }
+
+    ~Derived2Object() {
+
+        std::cout << "*** Destroying Derived2 Object " << mName << std::endl;
+    }
+
+    void greet() const
+    {
+        std::cout << mName << " says hi derived 2" << std::endl;
     }
 };
 
 int main()
 {
     VerboseObject jim("Jim");
-    VerboseObject sally("Sally");
+    DerivedObject *sally = NULL;
+    sally = new Derived2Object("Sally");
 
     std::cout << "--- Entering loop ---" << std::endl;
     for (int i = 0; i < 3; i++) 
@@ -42,11 +102,12 @@ int main()
         VerboseObject bubba("Bubba");
         bubba.greet();
 
-        sally.greet();
+        sally->greet();
 
         // end of for-loop scope; destructor called for bubba
     }
     std::cout << "--- Finishing loop ---" << std::endl;
 
+    delete sally;
     // end of main; destructors called for sally and jim
 }
