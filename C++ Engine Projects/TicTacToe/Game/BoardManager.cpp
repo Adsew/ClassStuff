324efd7e8c7646820c144bb29058113a1383df26
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-02 11:06:49 -0500

    work

--- C++ Engine Projects/TicTacToe/Game/BoardManager.cpp
+++ C++ Engine Projects/TicTacToe/Game/BoardManager.cpp
@@ -1,12 +1,20 @@
+/*
+Student: James Brooks
+Class: Game Architecture
+
+File: BoardManager.cpp
+
+Class: BoardManager
+
+Description: Controls the board and manages the aplyer's turns.
+*/
+
+
+#include "Core.h"
+
 #include "BoardManager.h"
 
 
-
-BoardManager::BoardManager()
-{
-}
+IMPLEMENT_COMPONENT(BoardManager);
 
 
-BoardManager::~BoardManager()
-{
-}
--- C++ Engine Projects/TicTacToe/Game/BoardManager.h
+++ C++ Engine Projects/TicTacToe/Game/BoardManager.h
@@ -1,8 +1,49 @@
-#pragma once
-class BoardManager
-{
+#ifndef BOARD_MANAGER_H
+#define BOARD_MANAGER_H
+
+/*
+Student: James Brooks
+Class: Game Architecture
+
+File: BoardManager.h
+
+Class: BoardManager
+
+Description: Controls the board and manages the aplyer's turns.
+*/
+
+
+#include "Component.h"
+
+
+class Player;
+
+
+class BoardManager : public Component {
+
+    REGISTER_COMPONENT(BoardManager);
+
+
+    /***** Variables *****/
+
+    Player *players[2];
+
+    bool player1sTurn;
+
+
+    /***** Functions *****/
+
+protected:
+
+    virtual void initialize() override;
+
+    virtual void update() override;
+
+    virtual void load(std::unique_ptr<FileSystem::FileAccessor> &accessor) override;
+
 public:
-    BoardManager();
-    ~BoardManager();
+
+    virtual Component &operator=(const Component &comp) override;
 };
 
+#endif // !BOARD_MANAGER_H
