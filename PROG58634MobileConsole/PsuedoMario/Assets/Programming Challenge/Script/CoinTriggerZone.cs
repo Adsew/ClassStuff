2d7285bf842a88915c390241e1e880b698b19356
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-03 15:35:51 -0500

    Mobile challenge 1 complete

---
+++ PROG58634MobileConsole/PsuedoMario/Assets/Programming Challenge/Script/CoinTriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTriggerZone : MonoBehaviour {
+    
+    public void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "Player") {
+
+            ScoreManager.This.updateScore(1);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PROG58634MobileConsole/PsuedoMario/Assets/Programming Challenge/Script/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static ScoreManager This;
+
+    public Text scoreDisplay;
+
+    private int score;
+
+    // Use this for initialization
+    void Start () {
+		
+        if (This == null) {
+
+            This = this;
+        }
+	}
+	
+	public void updateScore(int amount) {
+
+        score += amount;
+
+        if (scoreDisplay != null) {
+
+            scoreDisplay.text = "Score: " + score;
+        }
+    }
+}
