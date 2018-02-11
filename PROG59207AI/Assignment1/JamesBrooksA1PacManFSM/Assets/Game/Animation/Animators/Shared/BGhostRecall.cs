f639ef673dfb4c975b955387668f307b76ed23c2
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-11 11:35:41 -0500

    AI a1 work

---
+++ PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/Animation/Animators/Shared/BGhostRecall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGhostRecall : StateMachineBehaviour {
+
+    private GhostController gControl;
+    private Transform myTrans;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        gControl = animator.GetComponent<GhostController>();
+        myTrans = animator.transform;
+
+        if (gControl != null) {
+
+            gControl.moveToLocation = gControl.ReturnLocation;
+
+            // Need to turn off or pacman will die touching eyes
+            CircleCollider2D col = animator.GetComponent<CircleCollider2D>();
+
+            if (col != null) {
+
+                col.enabled = false;
+            }
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        if (gControl != null && myTrans != null) {
+
+            if (myTrans.position.x == gControl.ReturnLocation.x 
+                && myTrans.position.y == gControl.ReturnLocation.y) {
+
+                animator.SetTrigger("IsHome");
+            }
+        }
+    }
+}
---
+++ PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/Animation/Animators/Shared/BGhostReviving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGhostReviving : StateMachineBehaviour {
+    
+    private float cageTimer = 0.0f;
+
+    public float timeTilRevive = 5.0f;
+    
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        cageTimer += Time.deltaTime;
+
+        if (cageTimer >= timeTilRevive) {
+
+            // Need to turn off or pacman will die touching eyes
+            CircleCollider2D col = animator.GetComponent<CircleCollider2D>();
+
+            if (col != null) {
+
+                col.enabled = true;
+            }
+
+            animator.SetBool("IsDead", false);
+        }
+    }
+}
