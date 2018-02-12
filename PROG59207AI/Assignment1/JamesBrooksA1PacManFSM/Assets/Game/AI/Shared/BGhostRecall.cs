3bccd69d05db1806865239c93ff32f4a6e2f3401
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-11 20:17:20 -0500

    ai a1

---
+++ PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/AI/Shared/BGhostRecall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+public class BGhostRecall : StateBehaviour {
+
+    private GhostController gControl;
+    private Transform myTrans;
+
+    void Start() {
+
+        gControl = GetComponent<GhostController>();
+        myTrans = transform;
+    }
+
+    void OnEnable() {
+
+        if (gControl != null) {
+
+            gControl.moveToLocation = gControl.ReturnLocation;
+
+            // Need to turn off or pacman will die touching eyes
+            CircleCollider2D col = GetComponent<CircleCollider2D>();
+
+            if (col != null) {
+
+                col.enabled = false;
+            }
+        }
+    }
+
+    void Update() {
+
+        if (gControl != null && myTrans != null) {
+
+            if (myTrans.position.x == gControl.ReturnLocation.x
+                && myTrans.position.y == gControl.ReturnLocation.y) {
+
+                //animator.SetTrigger("IsHome");
+            }
+        }
+    }
+}
---
+++ PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/AI/Shared/BGhostReviving.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+public class BGhostReviving : StateBehaviour {
+    
+    private float cageTimer = 0.0f;
+
+    public float timeTilRevive = 5.0f;
+
+    void OnEnable() {
+
+        cageTimer = 0.0f;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    void Update() {
+
+        cageTimer += Time.deltaTime;
+
+        if (cageTimer >= timeTilRevive) {
+
+            // Need to turn off or pacman will die touching eyes
+            CircleCollider2D col = GetComponent<CircleCollider2D>();
+
+            if (col != null) {
+
+                col.enabled = true;
+            }
+
+            //animator.SetBool("IsDead", false);
+        }
+    }
+}
