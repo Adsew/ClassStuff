75d6615651bbf5735620ff2ac4e0873923d8511b
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-16 07:54:36 -0500

    a1 test

--- PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/AI/Inky/BInkyChaseState.cs
+++ PROG59207AI/Assignment1/JamesBrooksA1PacManFSM/Assets/Game/AI/Inky/BInkyChaseState.cs
@@ -5,21 +5,48 @@ using BehaviourMachine;
 
 public class BInkyChaseState : BGhostChaseState {
 
-    protected PelletController[] pellets;
+    protected List<PelletController> pellets;
+
+    public int changeStratAtXPellets = 7;
 
     new protected void Awake() {
 
         base.Awake();
 
-        pellets = Object.FindObjectsOfType<PelletController>();
+        pellets = new List<PelletController>(Object.FindObjectsOfType<PelletController>());
     }
 
     new protected void Update() {
 
         // Inky will go wherever there are pallets left to try and beat the player to it
-        if (gControl != null) {
+        if (gControl != null && pellets.Count > changeStratAtXPellets) {
 
+            if (myTrans.position.x >= gControl.moveToLocation.x - 1 && myTrans.position.x <= gControl.moveToLocation.x + 1
+                && myTrans.position.y >= gControl.moveToLocation.y - 1 && myTrans.position.y <= gControl.moveToLocation.y + 1) {
 
+                int randPellet = 0;
+
+                do {
+
+                    if (pellets[randPellet] == null) {
+
+                        pellets.RemoveAt(randPellet);
+                    }
+
+                    randPellet = (int)(Random.value * pellets.Count);
+
+                } while (pellets[randPellet] == null && pellets.Count > 0);
+
+                if (pellets.Count > 0) {
+
+                    Vector3 nextLocation = pellets[randPellet].transform.localPosition;
+
+                    gControl.moveToLocation.x = nextLocation.x;
+                    gControl.moveToLocation.y = nextLocation.y + 1; // Pellet locations are 1 lower than actual location
+                }
+            }
+        }
+        else if (gControl != null) {
 
             gControl.moveToLocation = gControl.PacMan.position;
         }
