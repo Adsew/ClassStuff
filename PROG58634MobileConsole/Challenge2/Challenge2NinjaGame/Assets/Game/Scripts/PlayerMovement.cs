7b19fed12fc2304f23fc016a173a9f783df5cb90
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-17 13:44:16 -0500

    mobile challenge 2 work

--- PROG58634MobileConsole/Challenge2/Challenge2NinjaGame/Assets/Game/Scripts/PlayerMovement.cs
+++ PROG58634MobileConsole/Challenge2/Challenge2NinjaGame/Assets/Game/Scripts/PlayerMovement.cs
@@ -7,13 +7,18 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rbody = null;
     private Animator anim = null;
 
+    private bool isAttacking = false;
+
     public string hMoveAxis = "Horizontal";
     public string vMoveAxis = "Vertical";
 
     public string fireButton = "Fire1";
 
+    public float idleCutOff = 0.1f;
     public float xMove, yMove;
 
+    public float speed = 1.0f;
+    
 	// Use this for initialization
 	void Start () {
 
@@ -29,19 +34,38 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        xMove = Input.GetAxis(hMoveAxis);
-        yMove = Input.GetAxis(vMoveAxis);
         
-        if (rbody != null) {
+        // Ensures last direction is maintained for facing idle direction
+        if (Input.GetAxis(vMoveAxis) != 0 || Input.GetAxis(hMoveAxis) != 0) {
 
-            rbody.velocity = new Vector2(xMove, yMove);
+            yMove = Input.GetAxis(vMoveAxis);
+            xMove = Input.GetAxis(hMoveAxis);
         }
 
+        // Only move if your past an idle threshold, allowing maintaining direction without movement
+        if (rbody != null) {
+            if (xMove > idleCutOff || yMove > idleCutOff
+                || xMove < -idleCutOff || yMove < -idleCutOff) {
+
+                rbody.velocity = new Vector2(xMove, yMove) * speed;
+            }
+            else {
+
+                rbody.velocity = new Vector2(0, 0);
+            }
+        }
+        
         if (anim != null) {
 
             anim.SetFloat(hMoveAxis, xMove);
             anim.SetFloat(vMoveAxis, yMove);
+
+            if (Input.GetButtonDown(fireButton) && !isAttacking) {
+
+                isAttacking = true;
+
+                anim.SetTrigger(fireButton);
+            }
         }
     }
 }
