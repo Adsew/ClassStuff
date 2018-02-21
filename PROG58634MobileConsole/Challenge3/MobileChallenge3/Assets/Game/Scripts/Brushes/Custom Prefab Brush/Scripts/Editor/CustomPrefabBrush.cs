9a25aca361cc66f8fb42b21a5a26be93a0c4bb6f
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-21 13:54:48 -0500

    base assets added challenge 3

---
+++ PROG58634MobileConsole/Challenge3/MobileChallenge3/Assets/Game/Scripts/Brushes/Custom Prefab Brush/Scripts/Editor/CustomPrefabBrush.cs
@@ -0,0 +1,25 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+
+//namespace UnityEditor
+//{
+//	[CreateAssetMenu]
+//	[CustomGridBrush(false, true, false, "Custom Prefab Brush")]
+//	public class CustomPrefabBrush : GridBrushBase
+//	{
+//		public GameObject mPrefab;
+
+//		public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
+//		{
+//			// Instaitate the Prefab
+
+//			// Register the Undo for the new object created
+
+//			// Set the parent for the new object to the brush
+
+//			// Make sure the new object is in the correct position in relation to the grid (don't forget to offset the instance to the center)
+//		}
+//	}
+//}
---
+++ PROG58634MobileConsole/Challenge3/MobileChallenge3/Assets/Game/Scripts/Brushes/Prefab Brush/Scripts/Editor/PrefabBrush.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor
+{
+	[CreateAssetMenu]
+	[CustomGridBrush(false, true, false, "Prefab Brush")]
+	public class PrefabBrush : GridBrushBase
+	{
+		public GameObject mPrefab;
+		public int mZ;
+
+		public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
+		{
+			// Do not allow editing palettes
+			if (brushTarget.layer == 31)
+				return;
+
+			GameObject instance = (GameObject) PrefabUtility.InstantiatePrefab(mPrefab);
+			Undo.RegisterCreatedObjectUndo((Object)instance, "Paint Prefabs");
+			if (instance != null)
+			{
+				instance.transform.SetParent(brushTarget.transform);
+				instance.transform.position = grid.LocalToWorld(grid.CellToLocalInterpolated(new Vector3Int(position.x, position.y, mZ) + new Vector3(.5f, .5f, .5f)));
+			}
+		}
+
+		public override void Erase(GridLayout grid, GameObject brushTarget, Vector3Int position)
+		{
+			// Do not allow editing palettes
+			if (brushTarget.layer == 31)
+				return;
+
+			Transform erased = GetObjectInCell(grid, brushTarget.transform, new Vector3Int(position.x, position.y, mZ));
+			if (erased != null)
+				Undo.DestroyObjectImmediate(erased.gameObject);
+		}
+
+		private static Transform GetObjectInCell(GridLayout grid, Transform parent, Vector3Int position)
+		{
+			int childCount = parent.childCount;
+			Vector3 min = grid.LocalToWorld(grid.CellToLocalInterpolated(position));
+			Vector3 max = grid.LocalToWorld(grid.CellToLocalInterpolated(position + Vector3Int.one));
+			Bounds bounds = new Bounds((max + min)*.5f, max - min);
+
+			for (int i = 0; i < childCount; i++)
+			{
+				Transform child = parent.GetChild(i);
+				if (bounds.Contains(child.position))
+					return child;
+			}
+			return null;
+		}
+	}
+
+	[CustomEditor(typeof(PrefabBrush))]
+	public class PrefabBrushEditor : GridBrushEditorBase
+	{
+		private PrefabBrush prefabBrush { get { return target as PrefabBrush; } }
+
+		private SerializedProperty m_Prefabs;
+		private SerializedObject m_SerializedObject;
+
+		protected void OnEnable()
+		{
+			m_SerializedObject = new SerializedObject(target);
+			m_Prefabs = m_SerializedObject.FindProperty("mPrefab");
+		}
+
+		public override void OnPaintInspectorGUI()
+		{
+			m_SerializedObject.UpdateIfRequiredOrScript();
+				
+			EditorGUILayout.PropertyField(m_Prefabs, true);
+			m_SerializedObject.ApplyModifiedPropertiesWithoutUndo();
+		}
+	}
+}
