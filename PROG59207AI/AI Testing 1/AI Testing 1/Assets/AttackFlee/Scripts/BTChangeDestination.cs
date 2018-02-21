ea4770b21cc6bfeba21fba607804cbcbcba08bcc
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-21 12:07:13 -0500

    ai lesson

---
+++ PROG59207AI/AI Testing 1/AI Testing 1/Assets/AttackFlee/Scripts/BTChangeDestination.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+[NodeInfo(category = "General/Actions/")]
+public class BTChangeDestination : ActionNode {
+
+    private int curDestination = 0;
+
+    public GameObject[] destinations = new GameObject[0];
+
+    // Update is called once per frame
+    public override Status Update () {
+
+        if (destinations.Length > 0) {
+
+            curDestination = (curDestination + 1) % destinations.Length;
+
+            blackboard.GetGameObjectVar("Destination").Value = destinations[curDestination];
+        }
+        
+        return Status.Success;
+	}
+}
