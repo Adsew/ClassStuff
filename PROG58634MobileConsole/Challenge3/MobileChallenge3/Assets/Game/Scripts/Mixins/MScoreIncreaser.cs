94215bde2c378361ed6910c448651cb573c16a32
Adsew <30912391+Adsew@users.noreply.github.com>
2018-02-23 13:53:35 -0500

    challenge 3 ocmplete

---
+++ PROG58634MobileConsole/Challenge3/MobileChallenge3/Assets/Game/Scripts/Mixins/MScoreIncreaser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MScoreIncreaser : MonoBehaviour {
+
+    public int scoreWorth = 1;
+
+    public void increaseScore() {
+
+        ScoreManager.This.updateScore(scoreWorth);
+    }
+}
---
+++ PROG58634MobileConsole/Challenge3/MobileChallenge3/Assets/Game/Scripts/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static ScoreManager This = null;
+
+    public int maxScore = 5;
+
+    private Text text = null;
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+		
+        if (This == null) {
+
+            This = this;
+        }
+
+        if (text == null) {
+
+            text = GetComponent<Text>();
+        }
+	}
+	
+    public void updateScore(int toAdd) {
+
+        score += toAdd;
+
+        if (text != null) {
+
+            text.text = "Found: " + score + " / " + maxScore;
+        }
+    }
+}
