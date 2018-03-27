5a3fbe97d4f8fd8066afc3a36997589e6b437f0e
Adsew <30912391+Adsew@users.noreply.github.com>
2018-03-27 14:33:41 -0400

    ai a2 work

---
+++ PROG59207AI/Assignment2/JamesBrooksA2/Assets/Scripts/Behaviours/IsAtDestination.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class IsAtDestination : Conditional {
+
+    private NavMeshAgent agent = null;
+
+    public override void OnAwake() {
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public override TaskStatus OnUpdate() {
+
+        if (agent != null) {
+            if (agent.remainingDistance <= agent.stoppingDistance) {
+
+                return TaskStatus.Success;
+            }
+        }
+
+        return TaskStatus.Failure;
+    }
+}
---
+++ PROG59207AI/Assignment2/JamesBrooksA2/Assets/Scripts/Behaviours/IsListCountComparrison.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class IsListCountComparrison : Conditional {
+
+    public SharedObjectList list;
+    public SharedInt integer;
+
+    public enum Comparitor {
+
+        Equals,
+        GreaterThan,
+        GreaterThanEquals,
+        LessThan,
+        LessThanEquals
+    }
+
+    public Comparitor compareMethod = Comparitor.Equals;
+
+    public override TaskStatus OnUpdate() {
+
+        if (list != null) {
+
+            if (compareMethod == Comparitor.Equals && list.Value.Count == integer.Value) {
+
+                return TaskStatus.Success;
+            }
+            else if (compareMethod == Comparitor.GreaterThan && list.Value.Count > integer.Value) {
+
+                return TaskStatus.Success;
+            }
+            else if (compareMethod == Comparitor.GreaterThanEquals && list.Value.Count >= integer.Value) {
+
+                return TaskStatus.Success;
+            }
+            else if (compareMethod == Comparitor.LessThan && list.Value.Count < integer.Value) {
+
+                return TaskStatus.Success;
+            }
+            else if (compareMethod == Comparitor.LessThanEquals && list.Value.Count <= integer.Value) {
+
+                return TaskStatus.Success;
+            }
+        }
+
+        return TaskStatus.Failure;
+    }
+}
