1de90e8d89ab827f24ff7ccd182a29433cb6cabd
Adsew <30912391+Adsew@users.noreply.github.com>
2018-04-08 16:57:08 -0400

    augment work

---
+++ CoolTools/AugmentSystemTest/Augment System Test/Assets/AugmentSystem/Augment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class Augment {
+
+    protected internal AugmentBoard board = null;
+    protected internal bool needsToBeAdded = true;
+
+
+    protected internal float timer = 0.0f;
+    public float duration = 0.0f;
+
+    /// <summary>
+    /// Override this to allow interaction with curently applied augments.
+    /// </summary>
+    public virtual void OnInteraction(Augment other) { }
+
+    /// <summary>
+    /// Override this to perform an action when applied. 
+    /// Occurs after interacting with currently applied augments.
+    /// </summary>
+    public virtual void OnApply() { }
+
+    /// <summary>
+    /// Override this to perform some action every frame.
+    /// </summary>
+    public virtual void OnUpdate() { }
+
+    /// <summary>
+    /// Override to perform an action when this augment naturally expires
+    /// </summary>
+    public virtual void OnFinish() { }
+}
---
+++ CoolTools/AugmentSystemTest/Augment System Test/Assets/Game/Scripts/Augments/Applicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Applicator : MonoBehaviour {
+    
+    public FireAug any = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
